9caf01193dab5775b7b6f904402294589c659c20
N0952373 <N0952373@ntu.ac.uk>
2023-10-10 12:08:58 +0100

    BiggerNumber game success

--- app/src/main/java/com/example/biggernumber/MainActivity.java
+++ app/src/main/java/com/example/biggernumber/MainActivity.java
@@ -11,53 +11,58 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int num1, num2, score;
+    private int num1;
+    private int num2;
+    private int points;
+
+
+    public void clickButton1(View view) {
+        check(num1, num2);
+    }
+
+    public void clickButton2(View view) {
+        check(num2, num1);
+    }
+
+    private void check(int a, int b) {
+        if (a > b) {
+            points++;
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        } else {
+            points--;
+            Toast.makeText(this, "You are STUPID.", Toast.LENGTH_SHORT).show();
+        }
+
+        TextView pointsView = (TextView) findViewById(R.id.pointsTextView);
+        pointsView.setText("Points: " + points);
+        roll();
+    }
+
+    private void roll() {
+        // pick two random numbers
+        Random r = new Random();
+        num1 = r.nextInt(9);
+        num2 = r.nextInt(9);
+        while (num2 == num1) {
+            num2 = r.nextInt(9);
+        }
+
+        Button left = (Button) findViewById(R.id.buttonLeft);
+        left.setText("" + num1);     // "" + int -> converts int to String
+
+        Button right = (Button) findViewById(R.id.buttonRight);
+        right.setText("" + num2);
+
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         roll();
-        check(num1,num2);
     }
-
-    public void clickButton1 (View view){
-        check(num1, num2);
-    }
-    public void clickButton2 (View view){
-        check(num1, num2);
-    }
-
-    private void check(int a, int b){
-        if (a>b){
-            score++;
-            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
-        } else {
-            score--;
-            Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show();
-        }
-
-        TextView scoreView = (TextView) findViewById(R.id.scoreTextView);
-        scoreView.setText("Score: "+score);
-        roll();
-    }
-
-    private void roll(){
-        // pick 2 random numbers
-        Random r = new Random();
-        num1 = r.nextInt(9);
-        num2 = r.nextInt(9);
-        while (num2 == num1){
-            num2 = r.nextInt(9);
-        }
-        Button left = (Button)findViewById(R.id.buttonLeft);
-        left.setText(""+num1);
-
-        Button right = (Button) findViewById(R.id.buttonRight);
-        right.setText(""+num2);
-
-    }
-
-
-
 }
